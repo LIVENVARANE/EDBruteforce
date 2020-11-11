a567d5182da9049e24c28a1c8d8a848be3ec0e98
liven <ulucqs@gmail.com>
2020-11-12 00:08:51 +0100

    Initial Commit

---
+++ EDBruteforce/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  EDBruteforce
+//
+//  Created by LIVEN on 12/11/2020.
+//  Copyright © 2020 LIVEN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
